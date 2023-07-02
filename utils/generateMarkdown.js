4c7ebe562a1a41312eb3a1c30f5c673e952b016a
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-07-02 17:41:06 -0300

    added renderLicenseSection

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -51,7 +51,19 @@ return licenseLink;
 }
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
+
+  const licenseLink = renderLicenseLink(license);
+
+  return `
+## License
+
+${licenseLink}
+  `;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
