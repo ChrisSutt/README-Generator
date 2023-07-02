a597d79b09ba8c45356e9847d460b1175f9ab00a
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-07-02 11:45:15 -0300

    added question arry to index.js

--- index.js
+++ index.js
@@ -1,8 +1,54 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Enter the title of your project:',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter a brief description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Enter the installation instructions:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Enter the usage information:',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Enter the contributing guidelines:',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Enter the test instructions:',
+    },
+    {
+      type: 'input',
+      name: 'license',
+      message: 'Enter the project license:',
+    },
+    {
+      type: 'input',
+      name: 'author',
+      message: 'Enter the author name:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter the author email:',
+    },
+  ];
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
